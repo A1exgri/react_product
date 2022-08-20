3b63e53038d0446a998835375239a03a4d629ed0
a1ex_g@list.ru <Set1music>
2022-08-20 19:09:46 +0300

    Project

---
+++ src/hooks/products.ts
@@ -0,0 +1,31 @@
+import {useEffect, useState} from "react";
+import {IProduct} from "../models";
+import axios, {AxiosError} from "axios";
+
+export function useProducts () {
+    const [products, setProducts] = useState<IProduct[]>([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    function addProduct(product: IProduct) {
+        setProducts(prev => [...prev, product])
+    }
+
+    async function fetchProducts() {
+        try {
+            setLoading(true)
+            const resp = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+            setProducts(resp.data)
+            setLoading(false)
+        } catch (e) {
+            setLoading(false)
+            const error = e as AxiosError
+            setError(error.message)
+        }
+    }
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    return { products, error, loading, addProduct }
+}
---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, ad aliquam aspernatur atque aut deleniti, dolorum est exercitationem, facere fuga illum ipsa laudantium magni minima minus mollitia nam nesciunt nisi nulla officiis praesentium provident quisquam ratione suscipit unde velit vitae! Amet distinctio, enim est harum illum ipsa porro quisquam totam!</div>
+    )
+}
